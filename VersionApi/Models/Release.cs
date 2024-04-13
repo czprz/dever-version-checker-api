1ff71a7e37176b49d8fbad2865bc531b30baaaa9
Casper Overholm Elkrog <casper.overholm@elkrog.me>
2024-04-13 17:54:37 +0200

    Updated to dotnet 8

--- VersionApi/Models/Release.cs
+++ VersionApi/Models/Release.cs
@@ -2,14 +2,14 @@
 
 namespace VersionCheckApi.Models;
 
-public class Release
+public record Release
 {
-    public string Url { get; set; }
+    public string Url { get; set; } = string.Empty;
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
     
     [JsonPropertyName("tag_name")]
-    public string TagName { get; set; }
+    public string TagName { get; set; } = string.Empty;
     
     [JsonPropertyName("created_at")]
     public DateTime Created { get; set; }
